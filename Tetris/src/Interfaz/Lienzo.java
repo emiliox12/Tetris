7ba94e952475242c16f61a38c3213ba01f2fe218
emiliox12 <63889328+emiliox12@users.noreply.github.com>
2020-04-17 21:08:16 -0500

    Add files via upload

---
+++ Tetris/src/Interfaz/Lienzo.java
@@ -0,0 +1,30 @@
+package Interfaz;
+
+import javax.swing.JFrame;
+
+
+public class Lienzo extends JFrame {
+
+	public final static long serialVersionUID = 1L;
+	
+	public final static int VOID = 0;
+	public final static int RED = 1;
+	public final static int GREEN = 2;
+	public final static int BLUE = 3;
+	public final static int YELLOW = 4;
+	public final static int POURPLE = 5;
+	public final static int PINK = 6;
+	public final static int ORANGE = 7;
+	
+	/**
+	 * Matriz que tiene que pintar.
+	 */
+	private int[][] cuadrilla;
+	
+	public Lienzo(int pX, int pY)
+	{
+		cuadrilla[pX][pY];
+	}
+
+
+}
